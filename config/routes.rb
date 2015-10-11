3b5e7cc6a60f7a67dcb6039a23a8ac7c500a3e5b
dhp1211 <dhp1211@gmail.com>
2015-10-11 09:04:16 +0700

    Use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  # config.force_ssl = true
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
