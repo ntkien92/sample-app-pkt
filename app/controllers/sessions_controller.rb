0c75c5b9f9c7def5bb53e8f4df650b430c1f1483
NTKIEN92 <ntkien92@gmail.com>
2015-10-13 10:23:07 +0700

    [MODIFIED] - Finish Log in/ Log out
    Log in/Log out with session, remember
     On branch log-in-log-out
     Changes to be committed:
    	modified:   app/assets/javascripts/application.js
    	modified:   app/assets/stylesheets/custom.css.scss
    	modified:   app/controllers/sessions_controller.rb
    	modified:   app/controllers/users_controller.rb
    	modified:   app/helpers/sessions_helper.rb
    	modified:   app/models/user.rb
    	modified:   app/views/layouts/_header.html.erb
    	modified:   app/views/sessions/new.html.erb
    	new file:   db/migrate/20151013015726_add_remember_digest_to_users.rb
    	modified:   db/schema.rb
    	modified:   test/controllers/sessions_controller_test.rb
    	modified:   test/fixtures/users.yml
    	new file:   test/helpers/session_helper_test.rb
    	modified:   test/integration/users_login_test.rb
    	modified:   test/integration/users_signup_test.rb
    	modified:   test/models/user_test.rb
    	modified:   test/test_helper.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,21 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+      log_in_with_session user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
+  def log_in_with_session(user)
+    log_in user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    redirect_to user
+  end
+
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
