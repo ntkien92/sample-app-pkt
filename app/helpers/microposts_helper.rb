8440d5ee26a1c234c4463c68ce9598be8b421e26
NTKIEN92 <ntkien92@gmail.com>
2015-10-20 10:33:55 +0700

    [ADD] - Finish create, delete micropost with ajax  On branch new-feed-ajax  Changes to be committed: 	modified:   app/controllers/microposts_controller.rb 	modified:   app/helpers/microposts_helper.rb 	modified:   app/views/microposts/_micropost.html.erb 	new file:   app/views/microposts/_microposts.html.erb 	new file:   app/views/microposts/create.js.erb 	new file:   app/views/microposts/destroy.js.erb 	modified:   app/views/shared/_feed.html.erb 	modified:   app/views/shared/_micropost_form.html.erb 	modified:   app/views/shared/_user_info.html.erb 	modified:   app/views/static_pages/home.html.erb 	modified:   app/views/users/show.html.erb 	new file:   public/javascript/pagination.js 	new file:   public/uploads/tmp/1445249247-7280-5887/Screenshot_from_2015-09-20_20_38_41.png

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,3 +1,21 @@
 # MicropostsHelper
 module MicropostsHelper
+  def destroy_micropost_in_profile_link(micropost)
+    @micropost = micropost
+    @user = User.find(@micropost.user.id)
+    @microposts = @micropost.user.microposts.paginate(page: params[:page])
+  end
+
+  def micropost_render_ajax_when_create(micropost)
+    @micropost = micropost
+    if @micropost.save
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.js
+      end
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
 end
