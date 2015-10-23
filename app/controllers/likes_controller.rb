6f96cb2d1dda20dca4f3a2c1d9f5765a668289f4
NTKIEN92 <ntkien92@gmail.com>
2015-10-23 17:03:54 +0700

    [FIX] - Rubocop
     On branch like-micropost
     Changes to be committed:
    	modified:   app/controllers/likes_controller.rb
    	modified:   app/helpers/likes_helper.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,19 @@
 # Action when user like/unlike a micropost
 class LikesController < ApplicationController
+  include LikesHelper
   before_action :logged_in_user
 
   def create
     @micropost = Micropost.find(params[:micropost_id])
     current_user.like(@micropost)
     @feed_items = current_user.feed.paginate(page: params[:page])
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.js
-    end
+    respond_ajax
   end
 
   def destroy
     @micropost = Like.find(params[:id]).liked
     current_user.unlike(@micropost)
     @feed_items = current_user.feed.paginate(page: params[:page])
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.js
-    end
+    respond_ajax
   end
 end
